67a9a713304e5e646e5f295851816820379baa6f
roman-yagodin <roman.yagodin@gmail.com>
2021-05-24 10:39:38 +0300

    Initial commit

---
+++ TandemSoapTester/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace TandemSoapTester
+{
+    internal class Program
+    {
+        public static void Main (string[] args)
+        {
+            Console.Write ("Username:");
+            var username = Console.ReadLine ();
+            
+            Console.Write ("Password:");
+            var password = Console.ReadLine ();
+            
+            var soapTester = new TandemSoapTester ();
+            var response = soapTester.GetEnrollmentCampaignList (username, password);
+            
+            Console.WriteLine ("Response:");
+            Console.WriteLine (response);
+            Console.WriteLine ();
+        }
+    }
+}
---
+++ TandemSoapTester/TandemSoapTester.cs
@@ -0,0 +1,70 @@
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Net;
+using System.Xml;
+
+namespace TandemSoapTester
+{
+    public class TandemSoapTester
+    {
+        public string GetEnrollmentCampaignList (string username, string password)
+        {
+            var envelope = PrepareEnvelope (username, password);
+            
+            Console.WriteLine ("Request:");
+            Console.WriteLine (envelope);
+            Console.WriteLine ();
+            
+            var soapEnvelopeXml = new XmlDocument();
+            soapEnvelopeXml.LoadXml (envelope);
+            
+            var webRequest = CreateWebRequest ("http://tandem-test.volgau.com/services/EnrCampaignInfoService",
+                "getEnrollmentCampaignList");
+ 
+            using (var stream = webRequest.GetRequestStream()) {
+                soapEnvelopeXml.Save (stream);
+            }
+
+            try {
+                using (var response = webRequest.GetResponse ()) {
+                    using (var rd = new StreamReader (response.GetResponseStream ())) {
+                        return rd.ReadToEnd ();
+                    }
+                }
+            }
+            catch (WebException ex) {
+                using (var rd = new StreamReader (ex.Response.GetResponseStream ())) {
+                    return rd.ReadToEnd ();
+                }
+            }
+
+            return string.Empty;
+        }
+
+        HttpWebRequest CreateWebRequest (string url, string action)
+        {
+            var webRequest = (HttpWebRequest) WebRequest.Create (url);
+            webRequest.Headers.Add ("SOAPAction", action);
+            webRequest.ContentType = "text/xml;charset=\"utf-8\"";
+            webRequest.Accept = "text/xml";
+            webRequest.Method = "POST";
+
+            return webRequest;
+        }
+
+        public string PrepareEnvelope (string username, string password)
+        {
+            var envelope = File.ReadAllText ("envelope-template.xml");
+
+            envelope = envelope.Replace ("{wsu:Created}", DateTime.Now.ToUniversalTime ().ToString ("O"));
+            envelope = envelope.Replace ("{wsu:Expires}", DateTime.Now.AddMinutes (5).ToUniversalTime ().ToString ("O"));
+
+            envelope = envelope.Replace ("{wsse:Username}", username);
+            envelope = envelope.Replace ("{wsse:Password}", password);
+            
+            return envelope;
+        }
+
+    }
+}
