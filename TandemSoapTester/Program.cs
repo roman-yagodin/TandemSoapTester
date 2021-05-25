ec95ca9b9dee8705f9058bba5f4e315b54a5e185
roman-yagodin <roman.yagodin@gmail.com>
2021-05-25 10:15:35 +0300

    Second envelope template with wsse:Nonce

--- TandemSoapTester/Program.cs
+++ TandemSoapTester/Program.cs
@@ -11,9 +11,15 @@ namespace TandemSoapTester
             
             Console.Write ("Password:");
             var password = Console.ReadLine ();
-            
+
+            Test ("envelope-template-1.xml", username, password);
+            Test ("envelope-template-2.xml", username, password);
+        }
+
+        static void Test (string path, string username, string password)
+        {
             var soapTester = new TandemSoapTester ();
-            var response = soapTester.GetEnrollmentCampaignList ("envelope-template-1.xml", username, password);
+            var response = soapTester.GetEnrollmentCampaignList (path, username, password);
             
             Console.WriteLine ("Response:");
             Console.WriteLine (response);
--- TandemSoapTester/TandemSoapTester.cs
+++ TandemSoapTester/TandemSoapTester.cs
@@ -1,7 +1,8 @@
 using System;
-using System.Diagnostics;
 using System.IO;
 using System.Net;
+using System.Security.Cryptography;
+using System.Text;
 using System.Xml;
 
 namespace TandemSoapTester
@@ -11,18 +12,18 @@ namespace TandemSoapTester
         public string GetEnrollmentCampaignList (string path, string username, string password)
         {
             var envelope = PrepareEnvelope (path, username, password);
-            
+
             Console.WriteLine ("Request:");
             Console.WriteLine (envelope);
             Console.WriteLine ();
-            
-            var soapEnvelopeXml = new XmlDocument();
+
+            var soapEnvelopeXml = new XmlDocument ();
             soapEnvelopeXml.LoadXml (envelope);
-            
+
             var webRequest = CreateWebRequest ("http://tandem-test.volgau.com/services/EnrCampaignInfoService",
                 "getEnrollmentCampaignList");
- 
-            using (var stream = webRequest.GetRequestStream()) {
+
+            using (var stream = webRequest.GetRequestStream ()) {
                 soapEnvelopeXml.Save (stream);
             }
 
@@ -57,14 +58,26 @@ namespace TandemSoapTester
         {
             var envelope = File.ReadAllText (path);
 
-            envelope = envelope.Replace ("{wsu:Created}", DateTime.Now.ToUniversalTime ().ToString ("O"));
-            envelope = envelope.Replace ("{wsu:Expires}", DateTime.Now.AddMinutes (5).ToUniversalTime ().ToString ("O"));
+            var created = DateTime.Now.ToUniversalTime ();
+            var createdStr = created.ToString ("O");
 
+            var expires = created.AddMinutes (5);
+            var expiresStr = expires.ToString ("O");
+
+            var nonce = string.Empty;
+            if (envelope.Contains ("{wsse:Nonce}")) {
+                var nonceBytes = Encoding.UTF8.GetBytes (Guid.NewGuid ().ToString ());
+                Array.Resize (ref nonceBytes, 16);
+                nonce = Convert.ToBase64String (nonceBytes);
+            }
+
+            envelope = envelope.Replace ("{wsu:Created}", createdStr);
+            envelope = envelope.Replace ("{wsu:Expires}", expiresStr);
             envelope = envelope.Replace ("{wsse:Username}", username);
             envelope = envelope.Replace ("{wsse:Password}", password);
-            
+            envelope = envelope.Replace ("{wsse:Nonce}", nonce);
+
             return envelope;
         }
-
     }
 }
